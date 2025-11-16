2ac972f42112f0b8f4cd2e16cab5f7209ef32ed2
leekleak <leekleak@proton.me>
2025-11-16 12:03:00 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.leekleak.trafficlight"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
         base.archivesName = "$namespace-$versionName"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

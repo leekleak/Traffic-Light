05e9347b93a4805184df9758950e33cd64ff8cf5
leekleak <leekleak@proton.me>
2025-11-20 18:08:56 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.leekleak.trafficlight"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.2"
+        versionCode = 6
+        versionName = "2.3"
         base.archivesName = "$namespace-$versionName"
     }
     buildTypes {
